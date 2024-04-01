7207fba29d0b127ba5b4fc423e9218eadb9f3973
Denglvch <biza44@mail.ru>
2024-04-01 12:36:46 +0400

    try-sms_receive-all-view

--- main.py
+++ main.py
@@ -10,6 +10,28 @@ from starlette.responses import Response
 app = FastAPI()
 
 
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    # Получаем информацию о запросе
+    method = request.method
+    url = request.url
+    http_version = request.scope['http_version']
+    client = request.client
+    headers = dict(request.headers)
+
+    # Выводим информацию о запросе
+    logger.info('New request')
+    logger.info(f"Received {method} request on {url} (HTTP/{http_version}) from {client}")
+    logger.info("Headers:")
+    for header, value in headers.items():
+        logger.info(f"{header}: {value}")
+    logger.info('END request')
+
+    # Продолжаем обработку запроса
+    response = await call_next(request)
+
+    return response
+
 @app.post('/sms_receive')
 async def sms_receive(request: Request):
     logger.info(f'New SMS: {await request.json()}')
